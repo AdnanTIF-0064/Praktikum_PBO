5df281094b3baec1cc08fa5745476e0d7d6d6ea7
Count-TIF <tsaqifadnan@gmail.com>
2026-09-02 10:28:34 +0700

    Add files via upload

---
+++ Adnanproject1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adnanproject1;
+
+/**
+ *
+ * @author acer
+ */
+public class Adnanproject1 {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Nama : Ibrahim Adnan Tsaqif");
+        System.out.println("NPM : 2505060064");
+        System.out.println("Alamat : Polanharjo, Klaten");
+        System.out.println("NO Hp : 081215278370");
+    } 
+}
